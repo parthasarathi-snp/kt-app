70bbdcbda8c97f0cfef309601a66279b94ced78f
parthasarathi-snp <Parthasarathi.p@snp.com>
2026-02-26 06:05:43 +0000

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SNP Cloud Technologies, Inc.</h1>
+        <p>Your trusted partner for scalable cloud solutions.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
